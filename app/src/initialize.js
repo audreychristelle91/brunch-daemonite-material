66ef9067792b355b02ad70a7cc05fe095fca804d
Audrey MOHIMBOUABÉKA <audrey.mohimbouabeka@gif.medasys.fr>
2019-03-13 10:21:21 +0100

    changing markers size

--- app/src/initialize.js
+++ app/src/initialize.js
@@ -5,7 +5,7 @@ var app = {
     layers:{},
     markers:null,
     loadMap: function () {
-      var token = token; // replace with your Mapbox API Access token. Create a Mabpox account and find it on https://www.mapbox.com/studio/
+      var token =token; // replace with your Mapbox API Access token. Create a Mabpox account and find it on https://www.mapbox.com/studio/
         var map = L.map('map',{
                 maxZoom: 18,
                 minZoom: 4
@@ -32,7 +32,7 @@ var app = {
                 //customize marker
                 var redIcon = L.icon({
                   iconUrl: 'img/markers/icon.png',
-                  iconSize:     [25, 38],
+                  iconSize:     [15, 22],
                   iconAnchor:   [12, 55],
                   popupAnchor:  [-3, -76]
               });
